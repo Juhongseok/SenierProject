e92795767a09710a5676767f7a6ec6d7833e786b
JHS <wnghdtjr129@naver.com>
2022-04-17 14:34:56 +0900

    로그인 validation추가

---
+++ seniorProject/src/main/java/com/jhs/seniorProject/controller/SessionConst.java
@@ -0,0 +1,5 @@
+package com.jhs.seniorProject.controller;
+
+public class SessionConst {
+    public static final String LOGIN_USER = "loginUser";
+}
--- seniorProject/src/main/java/com/jhs/seniorProject/controller/form/LoginForm.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/controller/form/LoginForm.java
@@ -1,19 +1,15 @@
 package com.jhs.seniorProject.controller.form;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 
-@Getter @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class LoginForm {
 
-//    @NotEmpty(message = "아이디를 입력하세요")
+    @NotEmpty(message = "아이디를 입력하세요")
     private String userId;
-//    @NotEmpty(message = "비밀번호를 입력하세요")
+
+    @NotEmpty(message = "비밀번호를 입력하세요")
     private String password;
 }
