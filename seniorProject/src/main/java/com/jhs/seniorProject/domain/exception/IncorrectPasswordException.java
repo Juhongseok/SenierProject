8f43c6bbe8db78b1d9af1185711da025fc9210c8
JHS <wnghdtjr129@naver.com>
2022-04-26 13:40:33 +0900

    비밀번호 오류 에러

---
+++ seniorProject/src/main/java/com/jhs/seniorProject/domain/exception/IncorrectPasswordException.java
@@ -0,0 +1,7 @@
+package com.jhs.seniorProject.domain.exception;
+
+public class IncorrectPasswordException extends Exception {
+    public IncorrectPasswordException(String message) {
+        super(message);
+    }
+}
