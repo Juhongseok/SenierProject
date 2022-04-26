9b3f6f69799f225ed9b3638830098472beaca37b
JHS <wnghdtjr129@naver.com>
2022-04-26 13:42:56 +0900

    카카오 로그인 처리추가 및 로그인 비밀번호 에러체크 추가

--- seniorProject/src/main/java/com/jhs/seniorProject/controller/UserController.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/controller/UserController.java
@@ -1,19 +1,34 @@
 package com.jhs.seniorProject.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.jhs.seniorProject.controller.form.KakaoInfo;
+import com.jhs.seniorProject.controller.form.KakaoToken;
 import com.jhs.seniorProject.controller.form.LoginForm;
 import com.jhs.seniorProject.controller.form.SignUpForm;
 import com.jhs.seniorProject.domain.User;
 import com.jhs.seniorProject.domain.exception.DuplicatedUserException;
+import com.jhs.seniorProject.domain.exception.IncorrectPasswordException;
 import com.jhs.seniorProject.domain.exception.NoSuchUserException;
 import com.jhs.seniorProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
+
+import java.util.UUID;
 
 import static com.jhs.seniorProject.controller.SessionConst.*;
 
@@ -31,13 +46,16 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signUp(@Validated @ModelAttribute SignUpForm signUpForm, BindingResult bindingResult){
+    public String signUp(@Validated @ModelAttribute SignUpForm signUpForm, BindingResult bindingResult) {
         log.info("userController --> signUpForm: {}", signUpForm);
-
-        checkPasswordEquals(bindingResult, signUpForm.getPassword(), signUpForm.getPasswordCheck());
 
         if (bindingResult.hasErrors()) {
             log.info("bindResult = {}", bindingResult);
+            return "users/signUpForm";
+        }
+
+        if (!signUpForm.getPassword().equals(signUpForm.getPasswordCheck())) {
+            bindingResult.reject("NotCorrectPassword", "비밀번호가 일치하지 않습니다");
             return "users/signUpForm";
         }
 
@@ -47,6 +65,7 @@ public class UserController {
             userId = userService.join(signUpUser);
         } catch (DuplicatedUserException e) {
             log.error("userController.signUp error ", e);
+            bindingResult.reject("duplicateUser", "중복된 회원입니다.");
             return "users/signUpForm";
         }
 
@@ -69,15 +88,14 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String createLoginForm(@ModelAttribute("loginForm") LoginForm loginForm){
+    public String createLoginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
         return "users/loginForm";
     }
 
     @PostMapping("/login")
     public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpSession session,
-                        @RequestParam(defaultValue = "/") String redirectURL){
+                        @RequestParam(defaultValue = "/") String redirectURL) {
         log.info("userController --> user.id: {}, user.password: {}", loginForm.getUserId(), loginForm.getPassword());
-
         if (bindingResult.hasErrors()) {
             log.info("bindResult = {}", bindingResult);
             return "users/loginForm";
@@ -89,12 +107,78 @@ public class UserController {
             findLoginUser = userService.login(loginUser);
         } catch (NoSuchUserException e) {
             log.error("UserController.login error", e);
+            bindingResult.reject("NoSuchUser", "아이디가 일치하지 않습니다.");
+            return "users/loginForm";
+        } catch (IncorrectPasswordException e) {
+            log.error("UserController.login error", e);
+            bindingResult.reject("NotCorrectPassword", "비밀번호가 일치하지 않습니다");
             return "users/loginForm";
         }
 
         log.info("loginUser = {}", findLoginUser);
         session.setAttribute(LOGIN_USER, findLoginUser);
         return "redirect:" + redirectURL;
+    }
+
+    /**
+     * user.id = kakao.id
+     * user.name = kakao.properties.nickname
+     * user.password = UUID.randomUUID
+     */
+    @GetMapping("/kakao_login")
+    public String kakaoLogin(String code, HttpSession session) {
+        log.info("kakao login execute");
+        RestTemplate restTemplate1 = new RestTemplate();
+
+        //로그인 인가 토큰 받기
+        HttpHeaders headers1 = getHttpHeaders();
+        MultiValueMap<String, String> params1 = getBodyData(code);
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params1, headers1);
+        ResponseEntity<String> response = restTemplate1.exchange(
+                "https://kauth.kakao.com/oauth/token"
+                , HttpMethod.POST
+                , kakaoTokenRequest
+                , String.class
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+        KakaoToken token = transferJsonDataToDTO(mapper, response, KakaoToken.class);
+
+        log.info("kakao AccessToken : {}", token);
+        log.info("finish get kakao AccessToken");
+
+        //토근 사용해서 사용자 정보 받아오기
+        RestTemplate restTemplate2 = new RestTemplate();
+        HttpHeaders headers2 = getHttpHeaders();
+        headers2.add("Authorization", "Bearer " + token.getAccess_token());
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
+        ResponseEntity<String> response2 = restTemplate2.exchange(
+                "https://kapi.kakao.com/v2/user/me"
+                , HttpMethod.POST
+                , kakaoProfileRequest
+                , String.class
+        );
+
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        KakaoInfo info = transferJsonDataToDTO(mapper, response2, KakaoInfo.class);
+
+        String password = UUID.randomUUID().toString().substring(0, 8);
+        log.info("kakao id : {}, kakao nickName : {}, password : {}"
+                , info.getId(), info.getProperties().getNickname(), password);
+
+        //사용자 정보 회원가입 및 로그인처리
+        User kakaoUser = null;
+        try {
+            kakaoUser = new User(String.valueOf(info.getId()), password, info.getProperties().getNickname());
+            userService.join(kakaoUser);
+        } catch (DuplicatedUserException e) {
+            return "users/loginForm";
+        }
+
+        log.info("login Success");
+        log.info("loginUser = {}", kakaoUser);
+        session.setAttribute(LOGIN_USER, kakaoUser);
+        return "redirect:/";
     }
 
     @PostMapping("/logout")
@@ -105,14 +189,33 @@ public class UserController {
         }
         return "redirect:/";
     }
-
-    private void checkPasswordEquals(BindingResult bindingResult, String password, String passwordCheck) {
-        if (!password.equals(passwordCheck)) {
-            bindingResult.rejectValue("NotCorrectPassword", "비밀번호가 일치하지 않습니다");
-        }
-    }
-
     private boolean isLoginStatus(User user) {
         return user != null;
     }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        return headers;
+    }
+
+    private MultiValueMap<String, String> getBodyData(String code) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "102364fc8aaacc13b119bdf84ae58bcd");
+        params.add("redirect_uri", "http://localhost:8080/users/kakao_login");
+        params.add("code", code);
+        return params;
+    }
+
+    private <T> T transferJsonDataToDTO(ObjectMapper mapper, ResponseEntity<String>response, Class<T> className){
+        T t = null;
+        try {
+            t = mapper.readValue(response.getBody(), className);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
 }
