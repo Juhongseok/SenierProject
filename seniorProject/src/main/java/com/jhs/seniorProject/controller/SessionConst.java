85085ae5dc1c3cda890780a4c8e7469826d8d444
JHS <wnghdtjr129@naver.com>
2022-04-29 00:03:07 +0900

    카카오 토큰 세션 추가

--- seniorProject/src/main/java/com/jhs/seniorProject/controller/SessionConst.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/controller/SessionConst.java
@@ -2,4 +2,5 @@ package com.jhs.seniorProject.controller;
 
 public class SessionConst {
     public static final String LOGIN_USER = "loginUser";
+    public static final String KAKAO_TOKEN = "kakaoToken";
 }
