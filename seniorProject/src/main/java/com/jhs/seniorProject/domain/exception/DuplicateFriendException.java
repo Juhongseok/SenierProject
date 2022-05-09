b2075555ae53126f48e0780078b4fbeb13147630
JHS <wnghdtjr129@naver.com>
2022-05-09 17:31:11 +0900

    중복 친구 에러검증 추가

---
+++ seniorProject/src/main/java/com/jhs/seniorProject/domain/exception/DuplicateFriendException.java
@@ -0,0 +1,7 @@
+package com.jhs.seniorProject.domain.exception;
+
+public class DuplicateFriendException extends Exception {
+    public DuplicateFriendException(String message) {
+        super(message);
+    }
+}
