527ac1227660e73b5a5fde1a74bfa88c94519b6b
JHS <wnghdtjr129@naver.com>
2022-05-14 18:12:23 +0900

    자기 자신 추가 못하도록 검증로직 추가

--- seniorProject/src/main/java/com/jhs/seniorProject/service/FriendService.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/service/FriendService.java
@@ -24,30 +24,22 @@ public class FriendService {
     private final UserRepository userRepository;
 
     /**
-     * 친구추가 창에서 아이디를 기준으로 검색을 할 때 사용
-     * @param friendId
-     * @return 아이디를 가진 사용자
-     * @throws NoSuchUserException
-     */
-    public User findFriend(String friendId) throws NoSuchUserException {
-        return userRepository.findById(friendId).orElseThrow(() -> new NoSuchUserException(friendId + "의 유저가 없습니다."));
-    }
-    /**
      * 친구 추가
      * 1. 친구 추가 검증로직
      * 2-1. 오류가 없으면 친구추가
      * 2-2. 오류가 있으면 throw new DuplicatedFriendException
      * @param user session 저장된 로그인 유저
-     * @param friend 사용자가 검색한 유저
+     * @param friendId 사용자가 검색한 유저
      * @throws NoSuchUserException
      */
-    public void addFriend(User user, User friend) throws DuplicateFriendException {
+    public void addFriend(User user, String friendId) throws DuplicateFriendException, NoSuchUserException {
+        User friend = findFriend(friendId);
+        validateMyself(user.getId(), friendId);
         validateDuplicatedFriend(user, friend);
-        FriendId friendId = new FriendId(user.getId(), friend.getId());
-        Friend friendRelation = new Friend(friendId, user, friend);
+        FriendId friendCompositeId = new FriendId(user.getId(), friend.getId());
+        Friend friendRelation = new Friend(friendCompositeId, user, friend);
         friendRepository.save(friendRelation);
     }
-
     /**
      * 친구목록보기
      * @param user session 저장된 로그인 유저
@@ -58,6 +50,21 @@ public class FriendService {
         return friendRepository.findByUserId(user);
     }
 
+    /**
+     * 친구추가 창에서 아이디를 기준으로 검색을 할 때 사용
+     * @param friendId
+     * @return 아이디를 가진 사용자
+     * @throws NoSuchUserException
+     */
+    private User findFriend(String friendId) throws NoSuchUserException {
+        return userRepository.findById(friendId).orElseThrow(() -> new NoSuchUserException(friendId + "의 유저가 없습니다."));
+    }
+
+    private void validateMyself(String userId, String friendId) throws DuplicateFriendException {
+        if (userId.equals(friendId)) {
+            throw new DuplicateFriendException("나자신과는 친구관계를 맺을수 없습니다.");
+        }
+    }
     /**
      * 친구추가 검증
      * 친구관계가 이미 맺어진 상태인지 확인
