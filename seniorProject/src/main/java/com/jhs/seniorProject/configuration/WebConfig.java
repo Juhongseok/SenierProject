6b50c6a3ca59401f1d276ba8499802a4cd11bd27
JHS <wnghdtjr129@naver.com>
2022-05-14 18:10:45 +0900

    argumentresolver 등록

--- seniorProject/src/main/java/com/jhs/seniorProject/configuration/WebConfig.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/configuration/WebConfig.java
@@ -1,10 +1,14 @@
 package com.jhs.seniorProject.configuration;
 
+import com.jhs.seniorProject.argumentresolver.LoginUserArgumentResolver;
 import com.jhs.seniorProject.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -22,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**")
                 .addResourceLocations("classpath:/static/image/");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
     }
 }
