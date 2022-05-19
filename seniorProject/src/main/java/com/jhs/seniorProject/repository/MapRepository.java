36ff35c0092b9fd4798d564b5dc2aa31ff3a945f
JHS <wnghdtjr129@naver.com>
2022-05-19 18:35:24 +0900

    지도 리포지토리 생성

--- seniorProject/src/main/java/com/jhs/seniorProject/repository/MapRepository.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/repository/MapRepository.java
@@ -2,12 +2,16 @@ package com.jhs.seniorProject.repository;
 
 import com.jhs.seniorProject.domain.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MapRepository extends JpaRepository<Map, Long> {
     List<Map> findByModifiedBy(String modifiedBy);
+    Optional<Map> findByCreatedByAndPassword(String createBy, String password);
 
-    Optional<Map> findByModifiedByAndPassword(String modifiedBy, String password);
+    @Query("select m from Map m join fetch m.userMaps um where um.user.id = :userId")
+    List<Map> findAll(@Param("userId") String userId);
 }
