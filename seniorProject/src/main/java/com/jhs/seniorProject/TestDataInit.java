52bcbecaeaaa08d8973f98e5e67b85c6a3ef808b
JHS <wnghdtjr129@naver.com>
2022-06-03 11:09:36 +0900

    테스트 데이터 추가

--- seniorProject/src/main/java/com/jhs/seniorProject/TestDataInit.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/TestDataInit.java
@@ -1,17 +1,23 @@
 package com.jhs.seniorProject;
 
+import com.jhs.seniorProject.controller.form.SaveLocationForm;
 import com.jhs.seniorProject.domain.Friend;
+import com.jhs.seniorProject.domain.Map;
+import com.jhs.seniorProject.domain.SmallSubject;
 import com.jhs.seniorProject.domain.User;
+import com.jhs.seniorProject.domain.enumeration.BigSubject;
 import com.jhs.seniorProject.domain.exception.DuplicateFriendException;
 import com.jhs.seniorProject.domain.exception.DuplicatedUserException;
 import com.jhs.seniorProject.domain.exception.NoSuchUserException;
 import com.jhs.seniorProject.service.FriendService;
+import com.jhs.seniorProject.service.LocationService;
 import com.jhs.seniorProject.service.MapService;
 import com.jhs.seniorProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +26,7 @@ public class TestDataInit {
     private final UserService userService;
     private final FriendService friendService;
     private final MapService mapService;
+    private final LocationService locationService;
 
     @PostConstruct
     public void init() {
@@ -32,8 +39,13 @@ public class TestDataInit {
             userService.join(userC);
 
             friendService.addFriend(userA, "userB");
-            mapService.createMap(userA.getName(), userA);
-
+            Map map = mapService.createMap(userA.getName(), userA);
+            List<SmallSubject> smallSubjects = map.getSmallSubjects();
+            locationService.saveLocation(
+                    new SaveLocationForm(1.2, 1.2, "save location1", "location1"
+                            , BigSubject.TOGO, smallSubjects.get(0), map)
+                    ,userA.getId()
+            );
         } catch (DuplicatedUserException e) {
 
         } catch (NoSuchUserException e) {
