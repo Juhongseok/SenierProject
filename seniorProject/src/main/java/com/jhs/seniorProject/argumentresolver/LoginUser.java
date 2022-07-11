e5457c2fa263e618c454b20a4d95dc4b416c16d0
jhs <wnghdtjr129@navr.com>
2022-07-11 17:46:17 +0900

    Session에서 찾는 User DTO로 변경

---
+++ seniorProject/src/main/java/com/jhs/seniorProject/argumentresolver/LoginUser.java
@@ -0,0 +1,10 @@
+package com.jhs.seniorProject.argumentresolver;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginUser {
+    private String id;
+}
