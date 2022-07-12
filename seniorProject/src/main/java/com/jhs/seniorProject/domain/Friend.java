2ed56dd6c4ac144988fd92d9618d392e92afdc85
jhs <wnghdtjr129@navr.com>
2022-07-12 10:31:18 +0900

    isNew 재정의

--- seniorProject/src/main/java/com/jhs/seniorProject/domain/Friend.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/domain/Friend.java
@@ -2,6 +2,7 @@ package com.jhs.seniorProject.domain;
 
 import com.jhs.seniorProject.domain.compositid.FriendId;
 import lombok.*;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.*;
 
@@ -10,10 +11,9 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString(of = {"userId", "friendId"})
 @Table(name = "FRIEND")
-public class Friend {
+public class Friend implements Persistable<FriendId> {
 
     @EmbeddedId
     private FriendId id;
@@ -27,4 +27,24 @@ public class Friend {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "FRIEND_ID")
     private User friendId;
+
+    @Transient
+    private transient boolean persisted;
+
+    public Friend(FriendId id, User userId, User friendId) {
+        this.id = id;
+        this.userId = userId;
+        this.friendId = friendId;
+    }
+
+    @Override
+    public boolean isNew() {
+        return !persisted;
+    }
+
+    @PostPersist
+    @PostLoad
+    public void setPersisted(){
+        persisted = true;
+    }
 }
