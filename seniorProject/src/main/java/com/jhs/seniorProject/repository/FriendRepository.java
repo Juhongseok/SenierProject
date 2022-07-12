d8fac69fa1c4e80f580ccac7f03218375d6353b0
jhs <wnghdtjr129@navr.com>
2022-07-12 10:31:52 +0900

    친구 목록보기 in query로 두번나가던것 한번으로 최적화

--- seniorProject/src/main/java/com/jhs/seniorProject/repository/FriendRepository.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/repository/FriendRepository.java
@@ -4,10 +4,13 @@ import com.jhs.seniorProject.domain.Friend;
 import com.jhs.seniorProject.domain.User;
 import com.jhs.seniorProject.domain.compositid.FriendId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, FriendId> {
-    List<Friend> findByUserId(User userId);
-    Friend findByUserIdAndFriendId(User userId, User friendId);
+    @Query("select f from Friend f join fetch f.friendId friend where f.userId.id = :userId")
+    List<Friend> findByIdUserId(@Param("userId") String userId);
+    Friend findByIdUserIdAndIdFriendId(String userId, String friendId);
 }
