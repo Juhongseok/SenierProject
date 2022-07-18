a489cc41305b5252ee7d509cdcf6a01e40405a35
user.email <wnghdtjr129@naver.com>
2022-07-18 10:35:38 +0900

    [Create] 22.07.18-해당 지도에 저장된 위치 저장 주제  조회

--- seniorProject/src/main/java/com/jhs/seniorProject/repository/SmallSubjectRepository.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/repository/SmallSubjectRepository.java
@@ -3,9 +3,13 @@ package com.jhs.seniorProject.repository;
 import com.jhs.seniorProject.domain.Map;
 import com.jhs.seniorProject.domain.SmallSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface SmallSubjectRepository extends JpaRepository<SmallSubject, Long> {
-    List<SmallSubject> findByMap(Map map);
+    @Query("select sb from SmallSubject sb where sb.map.id = :mapId")
+    List<SmallSubject> findByMapId(@Param("mapId") Long mapId);
 }
