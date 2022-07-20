bc39d96c08e3bece3645bf811b411f374b6a5a87
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-20 14:34:10 +0900

    [Modify] 22.07.20-위치 아이디 정보 추가

--- seniorProject/src/main/java/com/jhs/seniorProject/service/responseform/LocationList.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/service/responseform/LocationList.java
@@ -5,12 +5,14 @@ import lombok.Data;
 
 @Data
 public class LocationList {
+    private Long locationId;
     private Double latitude;
     private Double longitude;
     private String name;
 
     @Builder
-    public LocationList(Double latitude, Double longitude, String name) {
+    public LocationList(Long locationId, Double latitude, Double longitude, String name) {
+        this.locationId = locationId;
         this.latitude = latitude;
         this.longitude = longitude;
         this.name = name;
