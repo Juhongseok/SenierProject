4f310115851b36f04ca81c3e2bda6682e0822cdf
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-21 16:10:54 +0900

    [Modify] 22.07.21-커스텀 리포지토리 상속

--- seniorProject/src/main/java/com/jhs/seniorProject/repository/LocationRepository.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/repository/LocationRepository.java
@@ -2,13 +2,14 @@ package com.jhs.seniorProject.repository;
 
 import com.jhs.seniorProject.domain.Location;
 import com.jhs.seniorProject.domain.Map;
+import com.jhs.seniorProject.repository.customRepository.LocationRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long>, LocationRepositoryCustom {
 
     @Query("select l from Location l where l.map = :mapId")
     List<Location> findLocationsByMapId(@Param("mapId") Map mapId);
