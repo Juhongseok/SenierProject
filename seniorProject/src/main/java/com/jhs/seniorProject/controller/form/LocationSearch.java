433e23f6fb0fba0ce72f90dceadfcd9bf56ebcfe
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-22 15:00:34 +0900

    [Modify] 22.07.22-Controller에 enum 값 아닌 String 값 받도록 변경

--- seniorProject/src/main/java/com/jhs/seniorProject/controller/form/LocationSearch.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/controller/form/LocationSearch.java
@@ -1,6 +1,5 @@
-package com.jhs.seniorProject.service.responseform;
+package com.jhs.seniorProject.controller.form;
 
-import com.jhs.seniorProject.domain.enumeration.BigSubject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +10,6 @@ import lombok.NoArgsConstructor;
 public class LocationSearch {
     private Long mapId;
     private String name;
-    private BigSubject bigSubject;
+    private String bigSubject;
     private String smallSubject;
 }
