6fa8144e08fe56269a2d23c629c364609dc0e8ab
wnghdtjr129 <wnghdtjr129@naver.com>
2022-07-25 16:48:01 +0900

    [Modify] 22.07.25-paging 쿼리 in 절 분리

--- seniorProject/src/main/java/com/jhs/seniorProject/service/UserService.java
+++ seniorProject/src/main/java/com/jhs/seniorProject/service/UserService.java
@@ -1,7 +1,9 @@
 package com.jhs.seniorProject.service;
 
 import com.jhs.seniorProject.argumentresolver.LoginUser;
+import com.jhs.seniorProject.domain.UserMap;
 import com.jhs.seniorProject.repository.MapRepository;
+import com.jhs.seniorProject.repository.UserMapRepository;
 import com.jhs.seniorProject.service.requestform.LoginForm;
 import com.jhs.seniorProject.service.requestform.SignUpForm;
 import com.jhs.seniorProject.domain.User;
@@ -14,9 +16,13 @@ import com.jhs.seniorProject.service.responseform.UserMapList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -26,6 +32,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final MapRepository mapRepository;
+    private final UserMapRepository userMapRepository;
 
     public void join(SignUpForm signUpForm) throws DuplicatedUserException {
         User user = signUpForm.toEntity();
@@ -46,13 +53,29 @@ public class UserService {
         return new LoginUser(findUser.getId(), findUser.getName());
     }
 
+    /**
+     * 1. 조건 검색 및 페이징 처리 -> pk만 조회
+     * 2. pk 목록을 where in 으로 조회 및 fetch join
+     * 한방 쿼리로는 one to many 조인하면 무조건 full list fetch + in memory paging 발생
+     *
+     * @param id
+     * @param pageable
+     * @return
+     * @throws NoSuchUserException
+     */
     @Transactional(readOnly = true)
     public UserInfoResponse getUserInfo(String id, Pageable pageable) throws NoSuchUserException {
         User user = findUser(id);
 
-        Page<UserMapList> maps = mapRepository.findAll(pageable)
-                .map(map -> new UserMapList(map.getId(), map.getName(), map.getPassword()));
+        Page<UserMap> userMap = userMapRepository.findByIdUserId(user.getId(), pageable);
+        List<UserMapList> result = mapRepository.findMyMap(
+                        userMap.stream()
+                                .map(um -> um.getId().getMapId())
+                                .collect(Collectors.toList())
+                ).stream().map(m -> new UserMapList(m.getId(), m.getName(), m.getPassword()))
+                .collect(Collectors.toList());
 
+        PageImpl<UserMapList> maps = new PageImpl<>(result, pageable , userMap.getTotalElements());
         return UserInfoResponse.builder()
                 .id(id)
                 .password(user.getPassword())
